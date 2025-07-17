2b9bea7247c4b71aab2362ff504f93688275ebbe
tistakundu <tistakundu@microsoft.com>
2025-07-17 22:23:23 +0530

    Initial commit: React app with CDN setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading =React.createElement('h1', null, 'Hello, World!');
+const timepass= React.createElement('h2', null, 'This is a heading created by JS');
+const hi = React.createElement('h3', null, 'This is a heading created by Tista');
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+const mad =ReactDOM.createRoot(document.getElementById('mad'));
+
+root.render(timepass);
+
+mad.render(h3) ;
+
+
